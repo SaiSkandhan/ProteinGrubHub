7d8fe739e9e7caac7302efd809298f645564cee0
SaiSkandhan <saiskandhan9448@gmail.com>
2025-11-15 18:43:01 +0530

    Changes 3

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api',
-  googleClientId: '425797682801-fiqcsfg9pktesvn25e2ihpnbevsvlpf6.apps.googleusercontent.com',
+  googleClientId: '1015164201122-gs15qu8bc6t3adl32albif5tmvdg2pv0.apps.googleusercontent.com',
   appUrl: 'http://localhost:4201',  // Updated Angular app URL
   avatarApiEnabled: false,
   stripePublishableKey: 'pk_test_51SNZTQRuGCdDzhLHBatpmbyYwZFRgq9h5fAUcrT6sqD8kxKZTbkLGROtapnSlRWXL1rMEPYt0k6dXlgu5gFLXWwI00AqPuhRvQ'
