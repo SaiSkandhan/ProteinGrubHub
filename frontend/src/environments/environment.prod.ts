99c2e09fb5eafc3a2f4e0e83a579c4935feb951f
SaiSkandhan <saiskandhan9448@gmail.com>
2025-11-15 18:41:47 +0530

    Changes 3

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'https://proteingrubhub.onrender.com/api',
-  googleClientId: '425797682801-fiqcsfg9pktesvn25e2ihpnbevsvlpf6.apps.googleusercontent.com',
+  googleClientId: '1015164201122-gs15qu8bc6t3adl32albif5tmvdg2pv0.apps.googleusercontent.com',
   appUrl: 'https://proteinsgrubhub.vercel.app',
   avatarApiEnabled: false,
   stripePublishableKey: 'pk_test_51SNZTQRuGCdDzhLHBatpmbyYwZFRgq9h5fAUcrT6sqD8kxKZTbkLGROtapnSlRWXL1rMEPYt0k6dXlgu5gFLXWwI00AqPuhRvQ'
