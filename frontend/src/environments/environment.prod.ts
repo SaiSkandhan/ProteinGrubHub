0c24b8f04186914350e06c63174a2a7d6465b2d8
SaiSkandhan <125383968+SaiSkandhan@users.noreply.github.com>
2025-11-15 18:29:08 +0530

    Update production API and app URLs

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://protein-grub-hub.onrender.com/api',
+  apiUrl: 'https://proteingrubhub.onrender.com/api',
   googleClientId: '425797682801-fiqcsfg9pktesvn25e2ihpnbevsvlpf6.apps.googleusercontent.com',
-  appUrl: 'https://proteingrubhub.vercel.app',
+  appUrl: 'https://proteinsgrubhub.vercel.app',
   avatarApiEnabled: false,
   stripePublishableKey: 'pk_test_51SNZTQRuGCdDzhLHBatpmbyYwZFRgq9h5fAUcrT6sqD8kxKZTbkLGROtapnSlRWXL1rMEPYt0k6dXlgu5gFLXWwI00AqPuhRvQ'
 };
